093c2af3639e83e1c731d73a53f65ffd21f1dd3c
Johan Nilsson <oliver_twistor@hotmail.com>
2021-01-22 18:22:13 +0100

    Made minor changes in tests.

--- src/test/java/nu/olivertwistor/todolisttools/menus/AuthenticationTest.java
+++ src/test/java/nu/olivertwistor/todolisttools/menus/AuthenticationTest.java
@@ -1,30 +1,47 @@
 package nu.olivertwistor.todolisttools.menus;
 
-import nu.olivertwistor.todolisttools.rtmapi.Request;
 import nu.olivertwistor.todolisttools.util.Config;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.net.URL;
 
+/**
+ * Unit tests for the {@link Authentication} class.
+ *
+ * @since 1.0.0
+ */
 public final class AuthenticationTest
 {
     private static Config config;
-    private static Authentication authentication;
 
+    /**
+     * Creates a config object from test resources.
+     *
+     * @throws Exception if anything went wrong.
+     *
+     * @since 1.0.0
+     */
     @BeforeClass
     public static void setUp() throws Exception
     {
         final URL url = AuthenticationTest.class.getResource("/app.cfg");
         config = new Config(url);
-
-        authentication = new Authentication();
     }
 
+    /**
+     * Asserts that when generating an authentication request, no exception is
+     * thrown.
+     *
+     * @throws Exception if anything went wrong.
+     *
+     * @since 1.0.0
+     */
     @Test
     public void When_GeneratingAuthRequest_Then_NoExceptionIsThrown()
             throws Exception
     {
+        final Authentication authentication = new Authentication();
         authentication.generateAuthRequest(config, "write");
     }
 }
